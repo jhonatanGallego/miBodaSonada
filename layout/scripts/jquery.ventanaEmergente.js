a88c8876b84541887163db510597b9957a2c508b
Jhonatan <jonathangallegotorres@gmail.com>
2023-07-18 21:48:39 -0500

    modified:   .vscode/launch.json 	modified:   Antioquia.html 	modified:   Bogota.html 	modified:   Cauca.html 	modified:   Costa.html 	modified:   Meta.html 	modified:   Valle.html 	deleted:    images/MomentosFelices/HaciendaMaria.html 	deleted:    images/demo/100x100.png 	deleted:    images/demo/348x261.png 	deleted:    images/demo/348x400.png 	deleted:    images/demo/546x356.png 	new file:   images/demo/backgrounds/Antioquia.jpg 	new file:   images/demo/backgrounds/altarbodas.jpg 	new file:   images/demo/backgrounds/laMartina.jpg 	new file:   images/demo/backgrounds/lluvia_sobres.png 	new file:   images/demo/backgrounds/vestidos.jpg 	new file:   images/demo/besos.png 	new file:   images/demo/gallery/Bta/photo12.jpg 	new file:   images/demo/gallery/Bta/photo13.jpg 	new file:   images/demo/gallery/Bta/photo14.jpg 	renamed:    images/demo/gallery/Cta/photo_5006311518165379982_y.jpg -> images/demo/gallery/Cta/photo22.jpg 	renamed:    images/demo/gallery/Cta/photo03.mp4 -> images/demo/gallery/Cta/video02.mp4 	deleted:    images/demo/imgl.gif 	deleted:    images/demo/imgr.gif 	new file:   images/demo/perfil2.jpg 	new file:   images/demo/perfil_pao.jpg 	modified:   inicio.html 	modified:   layout/scripts/jquery.ventanaEmergente.js 	modified:   layout/styles/framework.css

--- layout/scripts/jquery.ventanaEmergente.js
+++ layout/scripts/jquery.ventanaEmergente.js
@@ -1,3 +1,64 @@
-function ventanaSecundaria (URL){ 
-   window.open(URL,"ventana1","width=120,height=300,scrollbars=NO") 
-} 
+const btnAbrirLugar = document.querySelector("#btn-abrir-lugar");
+const btnCerrarlugar = document.querySelector("#btn-cerrar-lugar");
+const lugar = document.querySelector("#lugar");
+
+btnAbrirLugar.addEventListener("click",()=>{
+   lugar.showModal();
+})
+
+btnCerrarlugar.addEventListener("click",()=>{
+   lugar.close();
+})
+
+
+const btnAbrirRegalo = document.querySelector("#btn-abrir-regalo");
+const btnCerrarRegalo = document.querySelector("#btn-cerrar-regalo");
+const regalo = document.querySelector("#regalo");
+
+btnAbrirRegalo.addEventListener("click",()=>{
+   regalo.showModal();
+})
+
+btnCerrarRegalo.addEventListener("click",()=>{
+   regalo.close();
+})
+
+
+const btnAbrirVestir = document.querySelector("#btn-abrir-vestir");
+const btnCerrarVestir = document.querySelector("#btn-cerrar-vestir");
+const vestir = document.querySelector("#vestir");
+
+btnAbrirVestir.addEventListener("click",()=>{
+   vestir.showModal();
+})
+
+btnCerrarVestir.addEventListener("click",()=>{
+   vestir.close();
+})
+
+
+
+const btnAbrirFecha = document.querySelector("#btn-abrir-fecha");
+const btnCerrarFecha = document.querySelector("#btn-cerrar-fecha");
+const fecha = document.querySelector("#fecha");
+
+btnAbrirFecha.addEventListener("click",()=>{
+   fecha.showModal();
+})
+
+btnCerrarFecha.addEventListener("click",()=>{
+   fecha.close();
+})
+
+
+const btnAbrirFechaCita = document.querySelector("#btn-abrir-cita");
+const btnCerrarFechaCita = document.querySelector("#btn-cerrar-cita");
+const cita = document.querySelector("#cita");
+
+btnAbrirFechaCita.addEventListener("click",()=>{
+   cita.showModal();
+})
+
+btnCerrarFechaCita.addEventListener("click",()=>{
+   cita.close();
+})
